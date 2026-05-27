f194ea12d2be7c99dcc43ee5cc33dbb42f030f97
Steven Gantz <steven.p.gantz@gmail.com>
2026-05-27 15:20:39 -0400

    push path change

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cesium from 'vite-plugin-cesium'
 
-export default defineConfig({
-  plugins: [react(), cesium()],
+// Change this to a function that receives the Vite environment context
+export default defineConfig(({ command }) => {
+  return {
+    // Only use the subpath base when building for production
+    base: command === 'build' ? '/projects/map/eldenring/' : '/',
+
+    plugins: [
+      react(),
+      cesium()
+    ],
+  }
 })
